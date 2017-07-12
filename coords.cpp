aaff166fc7eb4c6b0cb2a5f2e8ca1cd94d55f386
No0n3Left <no0n3left@gmail.com>
2017-07-12 13:20:15 -0500

    Working on area classes for area avoidance.

--- coords.cpp
+++ coords.cpp
@@ -2,4 +2,9 @@
 
 namespace Nav {
 	Vec2::Vec2(float _x, float _y) : x(_x), y(_y) { }
+	Vec2 Vec2::operator=(const Vec2& other) noexcept {
+		 x = other.x;
+		 y = other.y;
+		 return *this;
+	}
 };
--- nav.h
+++ nav.h
@@ -2,8 +2,57 @@
 #define _NAV_H 1
 
 #include <vector>
+#include <memory>
 
 namespace Nav {
+	struct Vec2 {
+		Vec2(float _x, float _y);
+		Vec2() = default;
+		float x, y;
+		Vec2 operator+(const Vec2& other);
+		Vec2 operator=(const Vec2& other) noexcept;
+	};
+	inline bool operator==(const Vec2& lhs, const Vec2& rhs) { return (lhs.x == rhs.x && lhs.y == rhs.y); }
+	inline bool operator!=(const Vec2& lhs, const Vec2& rhs) { return lhs != rhs; }
+
+	class Area {
+		public:
+			virtual ~Area() =0;
+			virtual bool contains(Nav::Vec2 point) =0;
+			virtual Nav::Area& intesects(const Area& other) =0;
+	};
+
+	class CircleArea: public Area {
+		public:
+			CircleArea(Nav::Vec2 _centre, float _radius);
+			virtual ~CircleArea();
+			virtual bool contains(Nav::Vec2 point);
+		private:
+			Nav::Vec2 centre;
+			float radius;
+	};
+
+	class PolyArea: public Area {
+		public:
+			virtual ~PolyArea();
+			virtual bool contains(Nav::Vec2 point);
+			virtual Nav::Area& intersects(const Area& other);
+		private:
+			std::vector<Nav::Vec2> points;
+	};
+
+	class Sensors {
+		
+	};
+
+	class Path {
+		public:
+			void setRestricted(const std::vector<std::unique_ptr<Nav::Area> > _restricted);
+			void compute();
+		private:
+			std::vector<std::unique_ptr<Nav::Area> > restricted;
+	};
+
 	class Navigator {
 		friend class Nav::Sensors;
 		friend class Nav::Path;
@@ -18,22 +67,8 @@ namespace Nav {
 			void runPath();
 		private:
 			std::vector<Nav::Vec2> posLog;
-			Path path;
+			Nav::Path path;
 	};
-
-	class Sensors {
-
-	};
-
-	class Path {
-
-	};
-
-	struct Vec2 {
-		Vec2(float _x, float _y);
-		Vec2() = default;
-		float x, y;
-	};
-};
+}
 
 #endif//_NAV_H
